e68ec513f7378a1983fe29ddb8d168e0a25b72f5
Jonathan Ogilvie <jo705@live.mdx.ac.uk>
2020-01-28 18:45:46 +0000

    End of development [No character controller or mouse look implemented]

---
+++ Assets/Scripts/PortalProximity.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalProximity : MonoBehaviour
+{
+    public GameObject Portal1;
+    public GameObject Portal2;
+    public GameObject Portal3;
+    public GameObject Portal4;
+    //Load our Portal game objects into Unity
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entering Portal");
+        //Outputs to the Unity console
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exiting Portal");
+        //Outputs to the Unity console
+    }
+}//End of script
---
+++ Assets/Scripts/playerReset.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerReset : MonoBehaviour
+{
+
+    public Transform spawn = null;
+    public CharacterController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (player.tag == "Player")
+        {
+            player.enabled = false;
+            other.transform.position = spawn.position;
+            player.enabled = true;
+            Debug.Log("Player reset");
+        }    
+    }
+}//end of script
